65a50b0583918ccef2cfbf7f2109664875730a1e
MetalFKingRawner <150486340+MetalFKingRawner@users.noreply.github.com>
2026-06-29 02:51:40 -0600

    Update settings.py

--- psycho_platform/settings.py
+++ psycho_platform/settings.py
@@ -205,7 +205,8 @@ LOGGING = {
 }
 
 # Añade al final del archivo
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+# Pon esto:
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 
 CLOUDINARY_STORAGE = {
